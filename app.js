7ebdddbd4f4e45f962f17455f1e04e21802d0875
mhdix <domanloom@gmail.com>
2024-06-14 01:42:29 -0700

    start

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
-const proListBtn = document.querySelector('.openModalBtn')
-const modal = document.querySelector("#myModal");
+// modal
+const cartBtn = document.querySelector('.openModalBtn')
+const cardModal = document.querySelector("#myModal");
+const backDrop = document.querySelector('.backdrop')
+const closeModalBtn = document.querySelector('.closeModal')
+import { productsData } from "./products.js";
 
-proListBtn.addEventListener('click', openModal)
+// get products
+class Products {
+
+    // get ftom API end point   
+    getProducts() {
+        return productsData;
+    }
+}
+
+// display products
+class UI {
+
+}
+
+// storage 
+class Storage {
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const products = new Products()
+    const productsData = products.getProducts()
+    console.log(productsData);
+})
+// cart item modal
 
 function openModal () {
-    console.log(modal.classList === "showModal" ? "has a class" : "not");
+    modal.style.display = 'block'
+    
 }
+function closeModal () {
+    modal.style.display = 'none'
+}
+cartBtn.addEventListener('click', openModal)
+closeModalBtn.addEventListener("click", closeModal);
+
+//  add product to card
---
+++ products.js
@@ -0,0 +1,26 @@
+export const productsData = [
+      {
+    id: "1",
+    title: "queen panel bed",
+    price: 10.99,
+    imageUrl: "./image/download.jpg",
+  },
+  {
+    id: "2",
+    title: "queen panel bed",
+    price: 16.99,
+    imageUrl: "./image/download1.jpg",
+  },
+  {
+    id: "3",
+    title: "queen panel bed",
+    price: 6.99,
+    imageUrl: "./image/download2.jpg",
+  },
+  {
+    id: "4",
+    title: "queen panel bed",
+    price: 2.69,
+    imageUrl: "./image/download3.jpg",
+  },
+];
